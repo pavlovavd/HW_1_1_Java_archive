d1f2fd05708108df2da2e5269dee5e95b29810a5
Victoria Pavlova <pvd0510@mail.ru>
2021-12-21 15:08:16 +0300

    Add Main Java

---
+++ Main/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int restofmoney = balance + money_transfer;
+        System.out.println("restogmoney");
+    }
+}
